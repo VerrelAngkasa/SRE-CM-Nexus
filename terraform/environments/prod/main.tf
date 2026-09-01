f5c7696916f1418613cc9d292a0497397ed7aea0
Verrel <verrel.angkasa@rsmurniteguh.com>
2026-09-01 13:55:35 +0700

    Create  google cloud storage module

---
+++ terraform/environments/prod/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.37.0, < 8.0.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "nexus-terraform"
+    prefix = "state"
+  }
+}
+
+provider "google" {
+    project = "sre-cm-nexus"
+    region = "asia-southeast2"
+}
+
+module "storage" {
+    source = "../../modules/storage"
+}
+
+module "compute" {
+    source = "../../modules/compute"
+}
---
+++ terraform/modules/storage/main.tf
@@ -0,0 +1,18 @@
+# Provision Google Cloud Storage Bucket
+locals {
+    project_id = "sre-cm-nexus"
+    bucket_name = "nexus-sre-bucket"
+}
+
+module "gcs_buckets" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  version = "~> 12.3"
+
+  project_id  = local.project_id
+  names       = [ local.bucket_name ]
+
+  set_admin_roles = true
+  admins = ["user:verrelangkasa20@gmail.com"]
+
+  versioning = { first = true }
+}
